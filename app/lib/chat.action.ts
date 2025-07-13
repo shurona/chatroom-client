b98308eb3f423b11c5164d9e17219d65ee5ca27e
shurona <khc4669@naver.com>
2025-07-13 17:06:04 +0900

    feat: 채팅창용 서버 axios 추가

--- app/lib/chat.action.ts
+++ app/lib/chat.action.ts
@@ -2,7 +2,9 @@
 
 import { axiosApiClient } from "@/app/api/utils/axios.utils"
 import { ServerResponse } from "@/app/types/response.type";
-import { ChatRoomResponseDto, ChatRoomCreateRequestDto, ChatRoomType } from "@/app/types/chat.type";
+import { ChatRoomResponseDto, ChatRoomCreateRequestDto, ChatRoomType, ChatContentType, ChatWriteRequestDto } from "@/app/types/chat.type";
+import { PageResponseType} from "@/app/types/response.type";
+import { ChatLogResponseDto } from "@/app/types/chat.type";
 
 /**
  * 서버에서 채팅방 목록을 가져오는 함수
@@ -40,4 +42,43 @@ export async function createPrivateChatRoom(accessToken: string, participantId: 
   };
 
   return await axiosApiClient.post<ChatRoomResponseDto>('/v1/chats/rooms/private', requestDto, accessToken);
+}
+
+/**
+ * 서버에서 채팅 로그를 가져오는 함수
+ */
+export async function fetchChatLogsFromServer(accessToken: string, chatRoomId: number, page = 0)
+: Promise<ServerResponse<PageResponseType<ChatLogResponseDto>>> {
+
+  if (!accessToken) {
+    console.error("Access token is required to fetch chat logs.");
+    return Promise.resolve({
+      success: false,
+      error: "Access token is required."
+    });
+  }
+
+  return await axiosApiClient.get(`/v1/chats/rooms/${chatRoomId}/logs?page=${page}`, accessToken);
+}
+
+/**
+ * 서버에 메시지를 보내는 함수
+ */
+export async function sendMessageToServer(accessToken: string, chatRoomId: number, content: string)
+: Promise<ServerResponse<number>> {
+  if (!accessToken) {
+    console.error("Access token is required to send a message.");
+    return Promise.resolve({
+      success: false,
+      error: "Access token is required."
+    });
+  }
+
+  const requestDto: ChatWriteRequestDto = {
+    chatRoomId,
+    message: content,
+    type: ChatContentType.TEXT 
+  };
+
+  return await axiosApiClient.post<number>(`/v1/chats/rooms/${chatRoomId}/messages`, requestDto, accessToken);
 }
--- app/types/chat.type.ts
+++ app/types/chat.type.ts
@@ -1,13 +1,38 @@
+// Chat room response DTO
 export interface ChatRoomResponseDto {
   id: number;
   name: string;
   roomType: ChatRoomType;
   lastMessage: string;
-  lastTime: string; // ISO 8601 format
+  lastTime: string; // 날짜가 넘어온다.
   memberCt: number;
   unreadCt: number;
-  createdAt: string; // ISO 8601 format
-  updatedAt: string; // ISO 8601 format
+  createdAt: string; // 날짜가 넘어온다.
+  updatedAt: string; // 날짜가 넘어온다.
+}
+
+// Chat room creation request DTO
+export interface ChatRoomCreateRequestDto {
+  friendUserIds: number[]; // For private chats, the ID of the participant
+  name?: string; // Optional name for group chats
+  roomType: ChatRoomType;
+}
+
+export interface ChatWriteRequestDto {
+  chatRoomId: number; // 채팅방 ID
+  message: string;
+  type: ChatContentType;
+}
+
+// Chat content types
+export interface ChatLogResponseDto {
+  id: number;
+  content: string;
+  type: ChatContentType;
+  writerId: number;
+  writeUserNickName: string; // 작성자 닉네임
+  wroteTime: string; // 날짜가 넘어온다.
+  unreadCount: number;
 }
 
 export enum ChatRoomType {
@@ -15,8 +40,8 @@ export enum ChatRoomType {
   PRIVATE = "PRIVATE"
 }
 
-export interface ChatRoomCreateRequestDto {
-  friendUserIds: number[]; // For private chats, the ID of the participant
-  name?: string; // Optional name for group chats
-  roomType: ChatRoomType;
+export enum ChatContentType {
+  TEXT = "TEXT",
+  // IMAGE = "IMAGE",
+  // FILE = "FILE",
 }
